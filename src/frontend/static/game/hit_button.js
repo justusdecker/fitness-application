8f48d9bcb9f13f8b5ead4964afde20f9f4f32890
Justus <justus.d2025@gmail.com>
2026-08-10 23:04:04 +0200

    rem querys and todo

--- src/frontend/static/game/hit_button.js
+++ src/frontend/static/game/hit_button.js
@@ -1,12 +1,3 @@
-/* 
-Add Suffix / Prefix
-Add _event_target
-Add _result_target
-Add _api_target
-*/
-function onClickHit() {
-
-}
 document.addEventListener("DOMContentLoaded", () => {
     // Wähle das Div anhand seiner Klasse aus
     const triggerDiv = document.querySelector(".hit-button");
@@ -21,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
             try {
                 // Anfrage an deine Flask-API (passe den Endpunkt an, z. B. '/api/daten')
                 const not_needed_response = await fetch("/api/increase?data=[coin]");
-                const response = await fetch("/api/get?data=[coin, material]");
+                const response = await fetch("/api/get");
 
                 if (!response.ok) {
                     throw new Error(`HTTP-Fehler! Status: ${response.status}`);

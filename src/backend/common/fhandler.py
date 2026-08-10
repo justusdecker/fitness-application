4332ee07acdc28ce0f6258c5a49c12e706ac92f4
Justus <justus.d2025@gmail.com>
2026-08-10 23:02:25 +0200

    add JSONLike type and yaml_read

--- src/backend/common/fhandler.py
+++ src/backend/common/fhandler.py
@@ -1,3 +1,14 @@
+from yaml import safe_load
+
+type JSONLike = dict | list
+
 def file_read(filepath : str) -> str:
     with open(filepath, 'r') as f:
         return f.read()
+
+def yaml_read(filepath : str, encoding='utf-8') -> JSONLike:
+    with open(
+        file = filepath, 
+        mode = 'r', 
+        encoding = encoding) as f:
+        return safe_load(f)

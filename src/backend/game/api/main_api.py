0c99550ed9d307a24bded371162cc1b9acb7a7dc
Justus <justus.d2025@gmail.com>
2026-08-11 18:56:14 +0200

    add api/quest-solve

--- src/backend/game/api/main_api.py
+++ src/backend/game/api/main_api.py
@@ -1,13 +1,29 @@
 from src.build_flask import *
+from time import time
 from src.backend.game.person import PERSON
+from src.backend.game.constants import QUESTS
+from src.backend.game.level_calculations import LevelAPI
 class GameAPI:
 
+    @app.route('/api/quest_solve/<key>')
+    def api_quest_solve(key: str):
+        PERSON.xp += QUESTS[key]['xp']
+        PERSON.coin += QUESTS[key]['coin'] if 'coin' in QUESTS[key] else 0
+        PERSON.material += QUESTS[key]['material'] if 'material' in QUESTS[key] else 0
+        PERSON.history.add(int(time()*1000), key)
+        PERSON.save()
+        return "", 200
     @app.route('/api/get')
     def api_get():
         return jsonify(
             {
-                'coin': PERSON.coin,
-                'material': PERSON.material
+                'coin': PERSON.formattedCoin,
+                'material': PERSON.formattedMaterial,
+                'xp': PERSON.xp,
+                'fxp': PERSON.formattedXP,
+                'level': PERSON.level,
+                'xp_max': PERSON.formattedXPMax,
+                'xp_percentage': PERSON.xpPercentage
             }
         )
     @app.route('/api/increase')

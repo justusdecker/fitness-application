e542987b458408ef89ca5d7aaeab8fd8b774a7b1
Justus <justus.d2025@gmail.com>
2026-08-11 18:54:14 +0200

    add get_xp_percentage

--- src/backend/game/level_calculations.py
+++ src/backend/game/level_calculations.py
@@ -10,3 +10,9 @@ class LevelAPI:
 
     def get_xp_max_for_current_level(xp: int):
         return (LevelAPI.get_current_level(xp) + 1) * 250
+    
+    def get_xp_percentage(xp: int):
+        xpbf = LevelAPI.get_xp_for_next_level(xp)
+        xpbf = xpbf / 250 if xpbf else 0
+        #xpbf *= 100
+        return xpbf

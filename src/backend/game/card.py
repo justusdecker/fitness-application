c31b9f529bc82cae4f702a976a7debc26064bbe8
Justus <justus.d2025@gmail.com>
2026-08-12 19:55:10 +0200

    add id, todict

--- src/backend/game/card.py
+++ src/backend/game/card.py
@@ -1,13 +1,27 @@
-
+from src.backend.game.format_number import format_number
 MAX_RARITY = 11
 class Card:
+    id = 0
+    
     def __init__(self, title, description,cost, img,rarity):
         self.title = title
         self.description = description
         self.cost = cost
         self.rarity = rarity
         self.img = img
-
+        self.id = Card.id
+        Card.id += 1
+    
+    def todict(self) -> dict:
+        return {
+            'title': self.title,
+            'description': self.description,
+            'cost': format_number(self.costForRarityLevel),
+            'rarity': self.rarityAsStr,
+            'img': self.img,
+            'id': self.id
+        }
+    
     def __debug(self):
         if self.description == '...': print(f"""\033[34m description of {self.title} is ...""")
         if self.cost == 0: print(f"""\033[34m cost of {self.title} is 0""")

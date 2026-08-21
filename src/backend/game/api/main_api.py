37214fdb5bf6f8852f4703a8b5a9537801773170
Justus <justus.d2025@gmail.com>
2026-08-21 23:14:55 +0200

    refac, add new route -> cardtowork

--- src/backend/game/api/main_api.py
+++ src/backend/game/api/main_api.py
@@ -3,7 +3,14 @@ from time import time
 from src.backend.game.person import PERSON
 from src.backend.game.constants import QUESTS
 from src.backend.game.level_calculations import LevelAPI
+from src.constants import http_status_codes
 class GameAPI:
+
+    @app.route('/api/update/cardtowork/<id>')
+    def api_set_card_to_work(id: int):
+        PERSON.setCardToWork(id)
+        PERSON.save()
+        return redirect('/'), 200
 
     @app.route('/api/quest_solve/<key>')
     def api_quest_solve(key: str):
@@ -12,7 +19,8 @@ class GameAPI:
         PERSON.material += QUESTS[key]['material'] if 'material' in QUESTS[key] else 0
         PERSON.history.add(int(time()*1000), key)
         PERSON.save()
-        return "", 200
+        return http_status_codes[200], 200
+    
     @app.route('/api/get')
     def api_get():
         return jsonify(
@@ -26,6 +34,7 @@ class GameAPI:
                 'xp_percentage': PERSON.xpPercentage
             }
         )
+        
     @app.route('/api/increase')
     def api_increase():
         data = request.args.get('data')

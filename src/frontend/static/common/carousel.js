773ff2aa8e022be60972a79a63ddf23db6b9a6b7
Justus <justus.d2025@gmail.com>
2026-08-21 23:13:44 +0200

    refac, add docs

--- src/frontend/static/common/carousel.js
+++ src/frontend/static/common/carousel.js
@@ -2,32 +2,32 @@ const cards = document.querySelectorAll('.quests');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 
-let currentIndex = 0;
+let currentIndex = 0; 
 
+/** 
+ * * Entfernt die 'active'-Klasse von allen Karten
+ * * Füge die 'active'-Klasse der neuen Karte hinzu
+ * * Gibt den Index in einer DebugMsg aus
+ * @param {string} index
+ * @return {void} none
+ * */  
 function showCard(index) {
-  // Entferne die 'active'-Klasse von allen Karten
   cards.forEach(card => card.classList.remove('active'));
-  
-  // Füge die 'active'-Klasse der neuen Karte hinzu
   cards[index].classList.add('active');
-  console.log(index);
+  console.log(`carousel:active -> ${index}`);
 }
 
 // Nächste Karte (mit Endlos-Loop)
 nextBtn.addEventListener('click', () => {
   currentIndex++;
-  if (currentIndex >= cards.length) {
-    currentIndex = 0; // Springt zurück zum Anfang
-  }
+  if (currentIndex >= cards.length) { currentIndex = 0; }
   showCard(currentIndex);
 });
 
 // Vorherige Karte (mit Endlos-Loop)
 prevBtn.addEventListener('click', () => {
   currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = cards.length - 1; // Springt zum Ende
-  }
+  if (currentIndex < 0) { currentIndex = cards.length - 1; }
   showCard(currentIndex);
 });
 cards[0].classList.add('active');
